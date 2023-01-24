bdf7d753b3731e58d3d995237f52b5ea135a034c
Dayglow0926 <dayglow0926@gmail.com>
2023-01-24 21:08:44 +0900

    add: 챌린지 추가

---
+++ challenge/christmas/src/index.js
@@ -0,0 +1,17 @@
+const clockTitle = document.querySelector(".js-clock");
+const christmasDate = new Date("2023", "11", "25", "00", "00", "00");
+
+function christmasCountDown() {
+  const nowDate = new Date();
+  const count = Math.abs(christmasDate.getTime() - nowDate.getTime());
+
+  const test = new Intl.RelativeTimeFormat("ko-KR").format(count, "second");
+  const date = Math.floor(count / 86400000);
+  const hour = Math.floor((count % 86400000) / 3600000);
+  const min = Math.floor((count % 3600000) / 60000);
+  const sec = Math.floor((count % 60000) / 1000);
+
+  clockTitle.innerHTML = `${date}d ${hour}h ${min}m ${sec}s`;
+}
+
+setInterval(christmasCountDown, 500);
